78c845730b54a9e955af10486371b0e84879c295
iskenovanargiza-cpu <iskenova.nargiza@gmail.com>
2025-12-12 21:10:10 -0500

    start

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -15,7 +15,7 @@ import java.util.List;
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
-
+@CrossOrigin
 public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
