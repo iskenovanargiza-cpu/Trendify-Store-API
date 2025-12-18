8b5e3a9a935b063b510b9c3348c6a0d7e29e6d9e
iskenovanargiza-cpu <iskenova.nargiza@gmail.com>
2025-12-18 17:57:56 -0500

    Phase 2 - BUG 1 corrected

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -24,8 +24,10 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
 
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
+                "   AND (price >= ? OR ? = -1) " +
                 "   AND (price <= ? OR ? = -1) " +
-                "   AND (subcategory = ? OR ? = '') ";
+                "   AND (subcategory = ? OR ? = '')"
+                ;
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
@@ -35,12 +37,18 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
         try (Connection connection = getConnection())
         {
             PreparedStatement statement = connection.prepareStatement(sql);
+
             statement.setInt(1, categoryId);
             statement.setInt(2, categoryId);
+
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, subCategory);
-            statement.setString(6, subCategory);
+
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+
+            statement.setString(7, subCategory);
+            statement.setString(8, subCategory);
 
             System.out.println(statement);
 
